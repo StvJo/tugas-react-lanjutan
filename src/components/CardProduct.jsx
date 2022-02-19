43796439ac080e4b6a3a6f03d709aa84d18ac36f
StvJo <sjon10422@gmail.com>
2022-02-19 21:42:18 +0700

    menambahkan files React Lanjutan Technical Assignment

---
+++ src/components/CardProduct.jsx
@@ -0,0 +1,45 @@
+import propTypes from "prop-types";
+
+const CardProduct = ({ name, image, price, addToCart }) => {
+  return (
+    <div className="card w-100">
+      <img src={image} className="card-img-top" alt="skilvul" />
+      <div className="card-body">
+        <h5 className="card-title">
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(price)}
+        </h5>
+        <h6 className="card-subtitle mb-2 text-muted text-capitalize">
+          {name}
+        </h6>
+        <button
+          type="button"
+          className="btn btn-primary w-100"
+          onClick={addToCart}
+        >
+          add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+CardProduct.propTypes = {
+  name: propTypes.string,
+  price: propTypes.number,
+  image: propTypes.string,
+  addToCart: propTypes.func,
+};
+
+CardProduct.defaultProps = {
+  name: "",
+  price: "",
+  image: "",
+  addToCart: () => {
+    return null;
+  },
+};
+
+export default CardProduct;
